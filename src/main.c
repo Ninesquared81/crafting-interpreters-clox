4a3bad5f314c107dfe79aa049d8ccd4f9d39f571
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2022-12-06 01:00:21 +0000

    Update main.c
    test new code

--- src/main.c
+++ src/main.c
@@ -6,11 +6,12 @@
 int main(int argc, const char *argv[]) {
     (void)argc; (void)argv;
     Chunk chunk;
-    init_chunk(&chunk);
+    init_chunk(&chunk);/*
     int constant = add_constant(&chunk, 1.2);
     write_chunk(&chunk, OP_CONSTANT, 123);
-    write_chunk(&chunk, constant, 123);
-
+    write_chunk(&chunk, constant, 123);*/
+    write_constant(&chunk, 1.2, 2);
+    write_constant(&chunk, 2.4, 123);
     write_chunk(&chunk, OP_RETURN, 123);
 
     disassemble_chunk(&chunk, "test chunk");
