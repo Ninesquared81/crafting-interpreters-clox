90863946627081bdfad87e35685f5e86ce0491ae
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2022-12-31 00:46:51 +0000

    chapter 17.2

--- src/compiler.c
+++ src/compiler.c
@@ -1,15 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
+#include "chunk.h"
 #include "scanner.h"
 
 typedef struct {
     Token current;
     Token previous;
+    bool had_error;
+    bool panic_mode;
 } Parser;
 
 Parser parser;
+
+static void error_at(Token *token, const char *message) {
+    if (parser.panic_mode) return;
+    parser.panic_mode = true;
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type == TOKEN_EOF) {
+        fprintf(stderr, " at end");
+    }
+    else if (token->type == TOKEN_ERROR) {
+
+    }
+    else {
+        fprintf(stderr, "at '%.*s'", token->length, token->start);
+    }
+
+    fprintf(stderr, ": %s\n", message);
+    parser.had_error = true;
+}
+
+static void error_at_current(const char *message) {
+    error_at(&parser.current, message);
+}
 
 static void advance(void) {
     parser.previous = parser.current;
@@ -21,9 +48,23 @@ static void advance(void) {
     }
 }
 
+static void consume(TokenType type, const char *message) {
+    if (parser.current.type == type) {
+        advance();
+        return;
+    }
+
+    error_at_current(message);
+}
+
 bool compile(const char *source, Chunk *chunk) {
     init_scanner(source);
+
+    parser.had_error = false;
+    parser.panic_mode = false;
+
     advance();
     expression();
     consume(TOKEN_EOF, "Expect end of expression.");
+    return !parser.had_error;
 }
