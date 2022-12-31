f0350ae8b34fef5f0ea9ff29355e8872b7858630
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2022-12-31 01:18:40 +0000

    Chapter 17.2

--- src/compiler.c
+++ src/compiler.c
@@ -14,6 +14,11 @@ typedef struct {
 } Parser;
 
 Parser parser;
+Chunk *compiling_chunk;
+
+static Chunk *current_chunk(void) {
+    return compiling_chunk;
+}
 
 static void error_at(Token *token, const char *message) {
     if (parser.panic_mode) return;
@@ -57,8 +62,26 @@ static void consume(TokenType type, const char *message) {
     error_at_current(message);
 }
 
+static void emit_byte(uint8_t byte) {
+    write_chunk(current_chunk(), byte, parser.previous.line);
+}
+
+static void emit_bytes(uint8_t byte1, uint8_t byte2) {
+    emit_byte(byte1);
+    emit_byte(byte2);
+}
+
+static void emit_return(void) {
+    emit_byte(OP_RETURN);
+}
+
+static void end_compiler(void) {
+    emit_return();
+}
+
 bool compile(const char *source, Chunk *chunk) {
     init_scanner(source);
+    compiling_chunk = chunk;
 
     parser.had_error = false;
     parser.panic_mode = false;
@@ -66,5 +89,6 @@ bool compile(const char *source, Chunk *chunk) {
     advance();
     expression();
     consume(TOKEN_EOF, "Expect end of expression.");
+    end_compiler();
     return !parser.had_error;
 }
