f491e428e6ab459c12416253c66fefcd33ee2afc
Ninesquared81 <lmspatriotdofs@gmail.com>
2023-04-29 02:19:38 +0100

    Chapter 23.4

--- src/compiler.c
+++ src/compiler.c
@@ -571,6 +571,56 @@ static void expression_statement(void) {
     emit_byte(OP_POP);
 }
 
+static void for_statement(void) {
+    begin_scope();
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'.");
+    if (match(TOKEN_SEMICOLON)) {
+        // No initializer.
+    }
+    else if (match(TOKEN_VAR)) {
+        var_declaration(true);
+    }
+    else if (match(TOKEN_VAL)) {
+        var_declaration(false);
+    }
+    else {
+        expression_statement();
+    }
+
+    int loop_start = current_chunk()->count;
+    int exit_jump = -1;
+    if (!match(TOKEN_SEMICOLON)) {
+        expression();
+        consume(TOKEN_SEMICOLON, "Expect ';' after loop condition.");
+
+        // Jump out of the loop if the condition is false.
+        exit_jump = emit_jump(OP_JUMP_IF_FALSE);
+        emit_byte(OP_POP);  // Condition
+    }
+
+    if (!match(TOKEN_RIGHT_PAREN)) {
+        int body_jump = emit_jump(OP_JUMP);
+        it increment_start = current_chunk()->count;
+        expression();
+        emit_byte(OP_POP);
+        consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+
+        emit_loop(loop_start);
+        loop_start = increment_start;
+        patch_jump(body_jump);
+    }
+
+    statement();
+    emit_loop(loop_start);
+
+    if (exit_jump != -1) {
+        patch_jump(exit_jump);
+        emit_byte(OP_POP);
+    }
+
+    end_scope();
+}
+
 static void if_statement(void) {
     consume(TOKEN_LEFT_PAREN, "Expect '(' after 'if'.");
     expression();
@@ -652,6 +702,9 @@ static void declaration(void) {
 static void statement(void) {
     if (match(TOKEN_PRINT)) {
         print_statement();
+    }
+    else if (match(TOKEN_FOR)) {
+        for_statement();
     }
     else if (match(TOKEN_IF)) {
         if_statement();
